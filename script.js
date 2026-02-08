f551fb0a9bceb130b8ba5290bf64b4c2da66fdf0
Trinoy Kundu <trinoykundu@gmail.com>
2026-02-08 13:53:07 +0600

    implement create task function

---
+++ script.js
@@ -0,0 +1,9 @@
+let createTask = function(task) {
+    let listItem = document.createElement('li');
+    let label = document.createElement('label');
+    let checkBox = document.createElement('input');
+
+    label.innerText = task;
+    checkBox.type = 'checkbox';
+    listItem.append(checkBox, label);
+}

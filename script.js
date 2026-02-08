dcc2705cdcb26436c63b002e8e86790d84ddef68
Trinoy Kundu <trinoykundu@gmail.com>
2026-02-08 14:22:56 +0600

    Implement addTask Function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let newTask = document.querySelector('#new-task');
+let todoUl = document.querySelector('#items')
+let form = document.querySelector('form');
+
 let createTask = function(task) {
     let listItem = document.createElement('li');
     let label = document.createElement('label');
@@ -6,6 +10,15 @@ let createTask = function(task) {
     label.innerText = task;
     checkBox.type = 'checkbox';
     listItem.append(checkBox, label);
-    
+
     return listItem
 }
+
+let addTask = function(event) {
+    event.preventDefault();
+    let listItem = createTask(newTask.value);
+    todoUl.appendChild(listItem);
+    listItem.value = "";
+}
+
+form.addEventListener('submit', addTask);
